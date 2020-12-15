eee1a6ada1e4e639556886a43868a4b2fe6142d4
luoyan52 <360031355@qq.com>
2020-12-15 21:49:44 +0800

    计算器作业，pytest ，参数化

--- conftest.py
+++ conftest.py
@@ -2,8 +2,7 @@ import pytest
 from  pythoncode.calculator import  Calculator
 
 @pytest.fixture(scope="class")
-def myfixture(self):
-    self.calc = Calculator()
+def myfixture():
     print("开始计算")
     yield
     print("结束计算")
--- test_calc.py
+++ test_calc.py
@@ -4,34 +4,36 @@ import yaml
 def get_datas(algo):
     with open('./data.yml', encoding='utf-8') as f:
         datas = yaml.safe_load(f)
-        datas = datas[algo]["datas"]
-        ids = datas[algo]["myid"]
-        return [datas, ids]
+        paras = datas[algo]['datas']
+        ids = datas[algo]['myid']
+        print(paras)
+        print(ids)
+        return [paras, ids]
 
 class TestCalc:
 
      @pytest.mark.parametrize("a,b,expect",
-                              get_datas("add")[0],
-                              ids=get_datas("add")[1])
+                              get_datas('add')[0],
+                              ids=get_datas('add')[1])
      def test_add(self,a,b,expect):
 
-         assert  expect== self.calc.add(a,b)
+         assert  expect== Calculator().add(a,b)
 
      @pytest.mark.parametrize("a,b,expect",
-                              get_datas("sub")[0],
-                              ids=get_datas("sub")[1])
+                              get_datas('sub')[0],
+                              ids=get_datas('sub')[1])
      def test_sub(self, a, b, expect):
 
-        assert expect == self.calc.sub(a, b)
+        assert expect == Calculator().sub(a, b)
 
      @pytest.mark.parametrize("a,b,expect",
-                              get_datas("mul")[0],
-                              ids=get_datas("mul")[1])
+                              get_datas('mul')[0],
+                              ids=get_datas('mul')[1])
      def test_mul(self, a, b, expect):
-         assert expect == self.calc.mul(a, b)
+         assert expect == Calculator().mul(a, b)
 
      @pytest.mark.parametrize("a,b,expect",
-                              get_datas("div")[0],
-                              ids=get_datas("div")[1])
+                              get_datas('div')[0],
+                              ids=get_datas('div')[1])
      def test_div(self, a, b, expect):
-         assert expect == self.calc.div(a, b)
+         assert expect == Calculator().div(a, b)
